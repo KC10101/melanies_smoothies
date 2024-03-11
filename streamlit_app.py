dab393f79b51559328dc7ce65e10dd1a805eaf5a
KC10101 <162986332+KC10101@users.noreply.github.com>
2024-03-11 11:40:12 +0000

    Update streamlit_app.py
    Adding requests code

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,4 +39,8 @@ if ingredient_list:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered!', icon="✅")
-        
+
+# New section to display fruityvice nutrition information
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
