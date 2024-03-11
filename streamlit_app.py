12a9c8784212a0a4bb386446fd8798344873cfc4
KC10101 <162986332+KC10101@users.noreply.github.com>
2024-03-11 12:28:22 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ if ingredient_list:
 
     #st.write(my_insert_stmt)
 
-    time_to_insert = st.button('Submit Order')
+            time_to_insert = st.button('Submit Order')
 
 if time_to_insert:
     session.sql(my_insert_stmt).collect()  
