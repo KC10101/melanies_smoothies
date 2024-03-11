c237749bc5d2a3596c4e258b46218ef26657f702
KC10101 <162986332+KC10101@users.noreply.github.com>
2024-03-11 11:30:57 +0000

    Update streamlit_app.py
    Latest changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,9 @@ from snowflake.snowpark.functions import col
 st.title(":kite: Customize Your Smoothie! :kite:")
 st.write("Choose the fruits you want in your custom Smoothie!")
 
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on your Smoothie will be:', name_on_order)
+
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
@@ -14,10 +17,10 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredient_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
+    , max_selections=5
 )
 
 if ingredient_list:
-
     ingredients_string = ''
 
     for fruit_chosen in ingredient_list:
@@ -26,10 +29,12 @@ if ingredient_list:
     #st.write(ingredients_string)
 
     
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
+     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
 
     #st.write(my_insert_stmt)
+    time_to_insert = st.button('Submit Order')
+
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
