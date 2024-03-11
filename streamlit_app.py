7205fc6778c94587fded1457e99fd10540535b2f
KC10101 <162986332+KC10101@users.noreply.github.com>
2024-03-11 11:32:10 +0000

    Update streamlit_app.py
    Latest

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,13 +27,11 @@ if ingredient_list:
         ingredients_string += fruit_chosen + ' '
 
     #st.write(ingredients_string)
-
     
      my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
 
     #st.write(my_insert_stmt)
-    time_to_insert = st.button('Submit Order')
 
     time_to_insert = st.button('Submit Order')
 
